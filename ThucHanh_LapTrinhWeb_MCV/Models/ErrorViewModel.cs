88bcfb5c315c191e47b9b5842a3cf0ca3ac104ad
Phong Tran <tp120607@gmail.com>
2026-09-21 19:34:14 +0700

    Add project files.

---
+++ ThucHanh_LapTrinhWeb_MCV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThucHanh_LapTrinhWeb_MCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
